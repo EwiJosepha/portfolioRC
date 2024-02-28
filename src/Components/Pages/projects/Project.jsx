45a81cd761f79ba1bb8038fcd569765cd575ab63
ewi josepha <ewijosepha@gmail.com>
2024-02-28 11:59:28 +0100

    adding portfolio

--- src/Components/Pages/projects/Project.jsx
+++ src/Components/Pages/projects/Project.jsx
@@ -1,42 +1,160 @@
-import React from 'react'
-import "./Projects.css"
+import React from "react";
+import "./Projects.css";
 
 function Project() {
   return (
     <div>
-          <div className="containerthumb2">
-          <div className="top">
-            <div className="subcard" id="subcards">
-              <img
-                src="https://media.istockphoto.com/id/1404894296/photo/woman-with-stylish-bag-on-grey-background-closeup.webp?b=1&s=170667a&w=0&k=20&c=2RyBB26QUkQTvJ3Vm2v9tH3ONzNCnwGomu0Wl5abN1U="
-                id="details-page"
-              />
+      <div className="containerthumb2">
+        <div className="top">
+          <div className="subcard" id="subcards">
+            <img
+              src="https://media.istockphoto.com/id/1404894296/photo/woman-with-stylish-bag-on-grey-background-closeup.webp?b=1&s=170667a&w=0&k=20&c=2RyBB26QUkQTvJ3Vm2v9tH3ONzNCnwGomu0Wl5abN1U="
+              id="details-page"
+            />
 
-              <i className="fa-regular fa-heart"></i>
-            </div>
-
-            <div className="snikersprice">
-              <span id="snykers">Versace</span>
-              <span id="snykers-price">$1200</span>
-            </div>
-
-            <div className="shoes-available">
-              <p id="shoes"> 5 types of shoes available</p>
-            </div>
-            <div className="stars">
-              <span id="star" className="fa-star"></span>
-              <p id="number">(121)</p>
-            </div>
-            <div className="date">
-              <button id="addtocard" className="addtocard">
-                Add to Card
-              </button>
-              <button id="shortlist">Short List</button>
-            </div>
+            {/* <i className="fa-regular fa-heart"></i> */}
+            <span style={{ color: "orangered" }}>E-Commerce Web Site</span>
           </div>
+
+          <div className="snikersprice">
+            {/* <span id="snykers">Versace</span>
+              <span id="snykers-price">$1200</span> */}
           </div>
+
+          <div className="shoes-available">
+            <p id="shoes">Awesome Project</p>
+          </div>
+          <div className="stars">
+            {/* <span id="star" className="fa-star"></span>
+              <p id="number"></p> */}
+          </div>
+          <div className="date">
+            <button id="addtocard" className="addtocard">
+              Project Details
+            </button>
+            <button id="shortlist">View project</button>
+          </div>
+        </div>
+        <div className="top">
+          <div className="subcard" id="subcards">
+            <img
+              src="https://images.unsplash.com/photo-1508736793122-f516e3ba5569?dpr=1&h=294&w=294&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8MzQ0OTcwOXx8ZW58MHx8fHx8"
+              id="details-page"
+            />
+
+            {/* <i className="fa-regular fa-heart"></i> */}
+            <span style={{ color: "orangered" }}>Meal Recipe</span>
+          </div>
+
+          <div className="snikersprice">
+            {/* <span id="snykers">Versace</span>
+              <span id="snykers-price">$1200</span> */}
+          </div>
+
+          <div className="shoes-available">
+            <p id="shoes">Awesome Project</p>
+          </div>
+          <div className="stars">
+            {/* <span id="star" className="fa-star"></span>
+              <p id="number"></p> */}
+          </div>
+          <div className="date">
+            <button id="addtocard" className="addtocard">
+              Project Details
+            </button>
+            <button id="shortlist">View project</button>
+          </div>
+        </div>
+        <div className="top">
+          <div className="subcard" id="subcards">
+            <img
+              src="https://media.istockphoto.com/id/183549820/photo/translation.webp?b=1&s=170667a&w=0&k=20&c=R2FpGXE0Va4Ds6uA8_WggZ-STvX4Yb_zpXWTaXniQJ8="
+            />
+
+            {/* <i className="fa-regular fa-heart"></i> */}
+            <span style={{ color: "orangered" }}>Language-Translator</span>
+          </div>
+
+          <div className="snikersprice">
+            {/* <span id="snykers">Versace</span>
+              <span id="snykers-price">$1200</span> */}
+          </div>
+
+          <div className="shoes-available">
+            <p id="shoes">Awesome Project</p>
+          </div>
+          <div className="stars">
+            {/* <span id="star" className="fa-star"></span>
+              <p id="number"></p> */}
+          </div>
+          <div className="date">
+            <button id="addtocard" className="addtocard">
+              Project Details
+            </button>
+            <button id="shortlist">View project</button>
+          </div>
+        </div>
+        <div className="top">
+          <div className="subcard" id="subcards">
+            <img
+              src="https://media.istockphoto.com/id/1011182136/de/foto/erledigte-aufgaben-auf-einer-to-do-liste-abhaken.jpg?s=1024x1024&w=is&k=20&c=0eHfGgjf-4EdST6Xeu_DCyonAgjRTfyqC6cK9qbSrbU="
+            />
+
+            {/* <i className="fa-regular fa-heart"></i> */}
+            <span style={{ color: "orangered" }}>Grocery-list</span>
+          </div>
+
+          <div className="snikersprice">
+            {/* <span id="snykers">Versace</span>
+              <span id="snykers-price">$1200</span> */}
+          </div>
+
+          <div className="shoes-available">
+            <p id="shoes">Awesome Project</p>
+          </div>
+          <div className="stars">
+            {/* <span id="star" className="fa-star"></span>
+              <p id="number"></p> */}
+          </div>
+          <div className="date">
+            <button id="addtocard" className="addtocard">
+              Project Details
+            </button>
+            <button id="shortlist">View project</button>
+          </div>
+        </div>
+        <div className="top">
+          <div className="subcard" id="subcards">
+            <img
+              src="https://media.istockphoto.com/id/1826452191/fr/photo/donn%C3%A9es-pr%C3%A9dictives-de-tableau-de-bord-kpi-financial-analytics.webp?b=1&s=170667a&w=0&k=20&c=2HRZaaBKeX4-TeryCxncSDo3Q4bixX9aTVtUsdVmsEQ="
+            />
+
+            {/* <i className="fa-regular fa-heart"></i> */}
+            <span style={{ color: "orangered" }}>User dash-Board</span>
+          </div>
+
+          <div className="snikersprice">
+            {/* <span id="snykers">Versace</span>
+              <span id="snykers-price">$1200</span> */}
+          </div>
+
+          <div className="shoes-available">
+            <p id="shoes">Awesome Project</p>
+          </div>
+          <div className="stars">
+            {/* <span id="star" className="fa-star"></span>
+              <p id="number"></p> */}
+          </div>
+          <div className="date">
+            <button id="addtocard" className="addtocard">
+              Project Details
+            </button>
+            <button id="shortlist">View project</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Project
+export default Project;
